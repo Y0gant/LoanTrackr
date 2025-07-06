d9b0d5f0c99dab69c6f07c84af8c01fc2aff7ff9
Y0gant <yogantfaye731@proton.me>
2025-07-07 03:09:30 +0530

    chore: initial project setup with Spring Boot, Maven, and base dependencies

---
+++ src/main/java/com/loantrackr/LoanTrackrApplication.java
@@ -0,0 +1,13 @@
+package com.loantrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanTrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanTrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loantrackr/LoanTrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loantrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanTrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
